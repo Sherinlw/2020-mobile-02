9eca03d86fa561fe5a14e8f029c3440f7f1cb19e
Sherinlw <Sherinlw1410@gmail.com>
2020-02-22 20:14:43 -0800

    Halaman Register

---
+++ app/src/main/java/id/putraprima/marketplacelayout/Register.java
@@ -0,0 +1,21 @@
+package id.putraprima.marketplacelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void clickNext(View view) {
+        Intent intent = new Intent(Register.this, Profile.class);
+        startActivity(intent);
+    }
+}
